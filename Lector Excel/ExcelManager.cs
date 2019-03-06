f8e33163400bd178728f739e57e55e2cd5f29eef
Marco Domínguez Anaya <mdominguezan@usal.es>
2019-03-06 22:33:32 +0100

    Agregar archivos de proyecto.

---
+++ Lector Excel/ExcelManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Excel = Microsoft.Office.Interop.Excel;
+namespace Lector_Excel
+{
+    public class ExcelManager
+    {
+        Excel.Application excelApp;
+        Excel.Workbook workbook;
+        Excel._Worksheet worksheet;
+        Excel.Range range;
+        public ExcelManager(String path)
+        {
+            excelApp = new Excel.Application();
+            workbook = excelApp.Workbooks.Open(path);
+        }
+
+        public void GetCellValues()
+        {
+            worksheet = workbook.Sheets[1];
+            range = worksheet.UsedRange;
+            int rows, columns;
+            rows = range.Rows.Count;
+            columns = range.Columns.Count;
+        }
+    }
+}
---
+++ Lector Excel/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lector_Excel
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ExcelManager ExcelManager;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Handles file opening button
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ExcelManager = new ExcelManager(openFileDialog.FileName);
+                menu_Export.IsEnabled = true;
+            }
+            else
+            {
+                menu_Export.IsEnabled = false;
+            }
+        }
+
+        //Handles data filling of Type 1
+        private void Menu_FillType1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Handles text file exporting
+        private void Menu_Export_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
