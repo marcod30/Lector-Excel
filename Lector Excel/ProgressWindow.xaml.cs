65a2c0e20c932a54e9b83c525aa49c550721bb60
marcod30 <mdominguezan@gmail.com>
2019-03-29 17:44:27 +0100

    Añadido texto con porcentaje de exportación

--- Lector Excel/ProgressWindow.xaml.cs
+++ Lector Excel/ProgressWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace Lector_Excel
 {
@@ -21,6 +12,14 @@ namespace Lector_Excel
     {
         bool isIndeterminate;
         string title = "Exportando...";
+
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
         public ProgressWindow(bool isIndeterminate, string title)
         {
             InitializeComponent();
@@ -39,6 +38,8 @@ namespace Lector_Excel
         {
             Export_Progressbar.IsIndeterminate = isIndeterminate;
             this.Title = title;
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
         }
     }
 }
