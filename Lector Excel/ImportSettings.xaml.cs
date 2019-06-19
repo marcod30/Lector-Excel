72dff7b5347a9115edbb66421917b4115a7893f8
marcod30 <mdominguezan@gmail.com>
2019-06-19 16:58:34 +0200

    Añadidos iconos a las barras de menú

--- Lector Excel/ImportSettings.xaml.cs
+++ Lector Excel/ImportSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,34 @@ namespace Lector_Excel
     /// </summary>
     public partial class ImportSettings : Window
     {
+        public List<string> positions { get; set; }
+
+        // Constructor
         public ImportSettings()
         {
             InitializeComponent();
+            positions = new List<string>();
+        }
+
+        // Called after triggering this window's close event
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            positions.Clear();
+            foreach(TextBox t in stack_text.Children.OfType<TextBox>())
+            {
+                if (t.IsEnabled)
+                {
+                    positions.Add(t.Text);
+                    Debug.WriteLine("Added " + t.Text);
+                }
+            }
+            //this.DialogResult = true;
+        }
+
+        // Handles manual window closing
+        private void Menu_CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
