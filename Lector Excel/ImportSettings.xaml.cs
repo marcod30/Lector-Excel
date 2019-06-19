808190244a31d1d112d6321c271044b8a85bf270
marcod30 <mdominguezan@gmail.com>
2019-06-19 18:06:02 +0200

    Añadida comprobación de vacíos y duplicados, añadido guardado local y reorganizado menú

--- Lector Excel/ImportSettings.xaml.cs
+++ Lector Excel/ImportSettings.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,25 +30,56 @@ namespace Lector_Excel
             positions = new List<string>();
         }
 
-        // Called after triggering this window's close event
-        private void Window_Closed(object sender, EventArgs e)
+        // Handles manual window closing, without saving changes
+        private void Menu_CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        // Handles manual window closing and saves changes
+        private void Menu_ConfirmAndClose_Click(object sender, RoutedEventArgs e)
         {
             positions.Clear();
-            foreach(TextBox t in stack_text.Children.OfType<TextBox>())
+            foreach (TextBox t in stack_text.Children.OfType<TextBox>())
             {
                 if (t.IsEnabled)
                 {
-                    positions.Add(t.Text);
+                    positions.Add(t.Text.ToUpper());
                     Debug.WriteLine("Added " + t.Text);
                 }
             }
-            //this.DialogResult = true;
+
+            if (positions.Count() != positions.Distinct().Count())   // Check if there are duplicates
+            {
+                MessageBox.Show("Parece que ha introducido valores duplicados. Por favor, revise los campos e inténtelo de nuevo", "Valores repetidos", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (!positions.All(s => !s.Equals("")))  // Check if any field was empty
+            {
+                MessageBox.Show("Parece que ha dejado algún campo vacío. Por favor, revise los campos e inténtelo de nuevo", "Campo vacío", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            this.DialogResult = true;
+            this.Close();
         }
 
-        // Handles manual window closing
-        private void Menu_CloseButton_Click(object sender, RoutedEventArgs e)
+        // Called after window is fully loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            if(positions != null && positions.Count > 0)
+            {
+                int i = 0;
+                foreach (TextBox t in stack_text.Children.OfType<TextBox>())
+                {
+                    if (t.IsEnabled)
+                    {
+                        t.Text = positions.ElementAt(i++);
+                    }
+                }
+
+                positions.Clear();
+            }
         }
     }
 }
