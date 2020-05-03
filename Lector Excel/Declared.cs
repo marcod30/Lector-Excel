f4ffc2902ebf259e164dd5bee56e2088bb881505
marcod30 <mdominguezan@gmail.com>
2020-05-03 21:18:02 +0200

    Añadida clase de estructura de datos de declarado

---
+++ Lector Excel/Declared.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lector_Excel
+{
+    public class Declared
+    {
+        bool isPropertyDeclared;
+        public Dictionary<string, string> declaredData;
+
+        //Constructor
+        public Declared()
+        {
+            declaredData = new Dictionary<string, string>();
+            declaredData.Add("DeclaredNIF", "");
+            declaredData.Add("LegalRepNIF", "");
+            declaredData.Add("CommunityOpNIF", "");
+            declaredData.Add("DeclaredName", "");
+            declaredData.Add("ProvinceCode", "");
+            declaredData.Add("CountryCode", "");
+            declaredData.Add("OpKey", "");
+            declaredData.Add("OpInsurance", "");
+            declaredData.Add("LocalBusinessLease", "");
+            declaredData.Add("OpIVA", "");
+            declaredData.Add("OpPassive", "");
+            declaredData.Add("OpCustoms", "");
+            declaredData.Add("TotalMoney", "");
+            declaredData.Add("AnualMoney", "");
+            declaredData.Add("AnualPropertyMoney", "");
+            declaredData.Add("AnualOpIVA", "");
+            declaredData.Add("Exercise", "");
+            declaredData.Add("TrimestralOp1", "");
+            declaredData.Add("TrimestralOp2", "");
+            declaredData.Add("TrimestralOp3", "");
+            declaredData.Add("TrimestralOp4", "");
+            declaredData.Add("AnualPropertyIVAOp1", "");
+            declaredData.Add("AnualPropertyIVAOp2", "");
+            declaredData.Add("AnualPropertyIVAOp3", "");
+            declaredData.Add("AnualPropertyIVAOp4", "");
+            isPropertyDeclared = false;
+        }
+
+        public Declared(Dictionary<string,string> dict, bool isProperty)
+        {
+            isPropertyDeclared = isProperty;
+            declaredData = dict;
+        }
+    }
+}
